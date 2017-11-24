14474e88ec645ee485531cec606147a7222df9cf
shibaty <shibaty@gmail.com>
2017-11-25 02:49:49 +0900

    supported detecting charset

--- ag2gpx.py
+++ ag2gpx.py
@@ -4,14 +4,18 @@
 
 import sys
 from datetime import datetime, timezone, timedelta
+from chardet.universaldetector import UniversalDetector
 
+# Usage
 CMD_PARAM_ERR="ag2gpx [from filename(srt)] [to filename(gpx)]"
 
+# AutoGuard's srt file TAG
 TAG_TIME = "時刻:"
 TAG_SPEED = "スピード:"
 TAG_LAT = "緯度:"
 TAG_LNG = "経度:"
 
+# GPX File Format
 GPX_HEADER = """
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <gpx xmlns="http://www.topografix.com/GPX/1/1"
@@ -35,8 +39,21 @@ GPX_TRKPT_FORMAT ="""
 </trkpt>
 """[1:]
 
+def charcode_detect_in_file(filename):
+    """detect character code in file"""
+    detector = UniversalDetector()
+    with open(filename, "rb") as file:
+        for line in file:
+            detector.feed(line)
+            if detector.done:
+                break
+    detector.close()
+    return detector.result['encoding']
+
 def main_routine():
     """main routine"""
+
+    # analyze parameter
     args = sys.argv
     if len(args) != 3:
         print(CMD_PARAM_ERR)
@@ -44,9 +61,14 @@ def main_routine():
     from_filename = args[1]
     to_filename = args[2]
 
-    with open(from_filename, "r") as from_file:
+    # detect charset
+    charset = charcode_detect_in_file(from_filename)
+
+    # read input
+    with open(from_filename, "r", encoding=charset) as from_file:
         from_lines = from_file.readlines()
-    
+
+    # convert    
     with open(to_filename, "w") as to_file:
         to_file.write(GPX_HEADER)
 
@@ -55,6 +77,7 @@ def main_routine():
             line = line.strip()
 
             if TAG_TIME in line:
+                # convert to utc timezone
                 time_string = line[len(TAG_TIME):].strip()
                 jst = timezone(timedelta(hours=+9), 'JST')
                 dt =  datetime.strptime(time_string, "%Y/%m/%d %H:%M:%S") \
@@ -70,6 +93,8 @@ def main_routine():
 
             if TAG_LNG in line:
                 point['lng'] = line[len(TAG_LNG):].strip()
+
+                # This TAG is last tag. flush to file.
                 trkpt = GPX_TRKPT_FORMAT.format(**point)
                 to_file.write(trkpt)
                 point = {}
